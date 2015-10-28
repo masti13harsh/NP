5558f8d70f45f688a26b5ff01cb111f010681bd2
masti13harsh <masti13harsh@gmail.com>
2015-10-29 00:39:06 +0530

    Initial Setup

---
+++ Desktop/NP-client/client/js/app.js
@@ -0,0 +1,41 @@
+var app = angular.module('NatkhatBharat', ['ui.router', 'ngMaterial', 'modelServices']);
+
+app
+.config(['$stateProvider', '$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+	    // default route
+	    $urlRouterProvider.otherwise('/index');
+
+	    // ui router states
+	    $stateProvider
+	        .state('index', {
+	            url: '/index',
+	            views: {
+	                header: {
+	                    templateUrl: 'views/index/header.html'
+	                },
+	                content: {
+	                    templateUrl: 'views/index/content.html'
+	            	}
+	            }
+	        })
+	        .state('index.login', {
+	            url: '/login',
+	            templateUrl: 'views/index/loginForm.html',
+	            controller: 'loginController'
+	        })
+	        .state('index.signUp', {
+	            url: '/signUp',
+	            templateUrl: 'views/index/signUpForm.html',
+	            controller: 'loginController'
+	        });
+}])
+.run(['$rootScope', '$state', function($rootScope, $state) {
+    $rootScope.$on('$stateChangeStart', function(event, next) {
+      // redirect to login page if not logged in
+      if (next.authenticate && !$rootScope.currentUser) {
+        event.preventDefault(); //prevent current page from loading
+        $state.go('index');
+      }
+    });
+}]);
---
+++ Desktop/NP-client/client/js/app/controllers/loginController.js
@@ -0,0 +1,18 @@
+app.controller('loginController', ['$scope', 'AuthService', '$state', 
+	function($scope, AuthService, $state){
+		console.log('Inside loginController');
+		$scope.user = {
+			email: '',
+			password: ''
+		};
+
+		$scope.loginAuth = function() {
+			console.log('email-->' + $scope.user.email + ' password-->' + $scope.user.password);
+			AuthService.login($scope.user.email, $scope.user.password)
+				.then(function() {
+					$state.go('home');
+				});
+		};
+
+	}]
+);
